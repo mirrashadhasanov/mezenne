66f01bf626cb5cc442f57cb788aa0e1371e5dfad
Mirrashad Hasanov <mirrashadhasanov@mail.ru>
2020-12-23 21:02:29 +0400

    Initial commit

---
+++ src/main/java/com/mezenne/HaciapiApplication.java
@@ -0,0 +1,13 @@
+package com.mezenne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaciapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaciapiApplication.class, args);
+	}
+
+}
